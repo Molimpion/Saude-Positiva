9f6822d2731207136be25405c3702439928858aa
Edmael Barreto <edmael_barreto@hotmail.com>
2025-11-29 14:02:10 -0300

    Implement CRUD operations for TestesAplicadosService

--- src/modules/testes-aplicados/testes-aplicados.service.ts
+++ src/modules/testes-aplicados/testes-aplicados.service.ts
@@ -1,1 +1,29 @@
+// testes-aplicados.service.ts
+import { prisma } from "../../shared/prisma";
+import { TesteAplicado } from "./testes-aplicados.entity";
+
+export class TestesAplicadosService {
+  async create(data: Omit<TesteAplicado, "id" | "criadoEm" | "atualizadoEm">) {
+    return prisma.testesAplicados.create({ data });
+  }
+
+  async findAll() {
+    return prisma.testesAplicados.findMany();
+  }
+
+  async findById(id: string) {
+    return prisma.testesAplicados.findUnique({ where: { id } });
+  }
+
+  async update(id: string, data: Partial<TesteAplicado>) {
+    return prisma.testesAplicados.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    return prisma.testesAplicados.delete({ where: { id } });
+  }
+}
 
