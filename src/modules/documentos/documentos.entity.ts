dc9a15d79ce7dd2f3eb98a6261fbd58d0cd179bc
Edmael Barreto <edmael_barreto@hotmail.com>
2025-11-29 14:04:29 -0300

    Add Documento interface for document entities

--- src/modules/documentos/documentos.entity.ts
+++ src/modules/documentos/documentos.entity.ts
@@ -1,1 +1,10 @@
+// documentos.entity.ts
+export interface Documento {
+  id: string;
+  titulo: string;
+  tipo: string;
+  caminhoArquivo: string; // URL ou path
+  criadoEm: Date;
+  atualizadoEm: Date;
+}
 
