d43a8a668bdfec1d07b53b1067468ae57eadc2ee
Edmael Barreto <edmael_barreto@hotmail.com>
2025-11-29 13:58:08 -0300

    Implement DiagnosticosService with CRUD methods

--- src/modules/diagnosticos/diagnosticos.service.ts
+++ src/modules/diagnosticos/diagnosticos.service.ts
@@ -1,1 +1,30 @@
+// diagnosticos.service.ts
+import { prisma } from "../../shared/prisma";
+import { Diagnostico } from "./diagnosticos.entity";
+
+export class DiagnosticosService {
+  async create(data: Omit<Diagnostico, "id" | "criadoEm" | "atualizadoEm">) {
+    return prisma.diagnostico.create({ data });
+  }
+
+  async findAll() {
+    return prisma.diagnostico.findMany();
+  }
+
+  async findById(id: string) {
+    const diagnostico = await prisma.diagnostico.findUnique({ where: { id } });
+    return diagnostico;
+  }
+
+  async update(id: string, data: Partial<Diagnostico>) {
+    return prisma.diagnostico.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    return prisma.diagnostico.delete({ where: { id } });
+  }
+}
 
