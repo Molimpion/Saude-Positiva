f5da96d45b421c780a3565a0c7cb2d41c5220f0b
Edmael Barreto <edmael_barreto@hotmail.com>
2025-11-29 14:01:51 -0300

    Update testes-aplicados.schema.ts

--- src/modules/testes-aplicados/testes-aplicados.schema.ts
+++ src/modules/testes-aplicados/testes-aplicados.schema.ts
@@ -1,1 +1,28 @@
+// testes-aplicados.schema.ts
+import { z } from "zod";
+
+export const CreateTesteAplicadoSchema = z.object({
+  body: z.object({
+    nome: z.string().min(3, "Nome obrigatório"),
+    descricao: z.string().min(5, "Descrição obrigatória"),
+    resultado: z.string().min(2, "Resultado obrigatório"),
+  }),
+});
+
+export const UpdateTesteAplicadoSchema = z.object({
+  body: z.object({
+    nome: z.string().min(3).optional(),
+    descricao: z.string().min(5).optional(),
+    resultado: z.string().min(2).optional(),
+  }),
+  params: z.object({
+    id: z.string().uuid(),
+  }),
+});
+
+export const ShowOrDeleteTesteAplicadoSchema = z.object({
+  params: z.object({
+    id: z.string().uuid(),
+  }),
+});
 
