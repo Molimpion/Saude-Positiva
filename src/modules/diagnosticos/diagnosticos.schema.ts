cbfa3394ecc172b70faab618e4d7c2dcddfb5925
Edmael Barreto <edmael_barreto@hotmail.com>
2025-11-29 13:57:37 -0300

    Update diagnosticos.schema.ts

--- src/modules/diagnosticos/diagnosticos.schema.ts
+++ src/modules/diagnosticos/diagnosticos.schema.ts
@@ -1,1 +1,26 @@
+// diagnosticos.schema.ts
+import { z } from "zod";
+
+export const CreateDiagnosticoSchema = z.object({
+  body: z.object({
+    titulo: z.string().min(3, "Título obrigatório"),
+    descricao: z.string().min(5, "Descrição obrigatória"),
+  }),
+});
+
+export const UpdateDiagnosticoSchema = z.object({
+  body: z.object({
+    titulo: z.string().min(3).optional(),
+    descricao: z.string().min(5).optional(),
+  }),
+  params: z.object({
+    id: z.string().uuid(),
+  }),
+});
+
+export const ShowOrDeleteDiagnosticoSchema = z.object({
+  params: z.object({
+    id: z.string().uuid(),
+  }),
+});
 
