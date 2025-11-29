319f10da7632030e4c70b4071c84cbbadd8c745d
Edmael Barreto <edmael_barreto@hotmail.com>
2025-11-29 14:01:31 -0300

    Add TesteAplicado interface to entity file

--- src/modules/testes-aplicados/testes-aplicados.entity.ts
+++ src/modules/testes-aplicados/testes-aplicados.entity.ts
@@ -1,1 +1,10 @@
+// testes-aplicados.entity.ts
+export interface TesteAplicado {
+  id: string;
+  nome: string;
+  descricao: string;
+  resultado: string;
+  criadoEm: Date;
+  atualizadoEm: Date;
+}
 
